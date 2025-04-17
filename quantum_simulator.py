6345f71cb4920d1203c195616b868c166172da2e
Aditya Patwardhan <adityap.thecool@gmail.com>
2025-04-17 07:57:39 -0500

    first commit

---
+++ quantum_simulator.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+"""
+This takes as input an OPENQASM 2.0 file and integer number of shots.
+It computes the quantum sate before measurement as a complex vector
+and produces the number of counts for each possible measurement outcome.
+"""
