7bb016dafdd7d1447d7ff8612d36b894766e3b82
Aditya Patwardhan <adityap.thecool@gmail.com>
2025-04-17 08:09:34 -0500

    Added gates

--- quantum_simulator.py
+++ quantum_simulator.py
@@ -5,3 +5,30 @@ This takes as input an OPENQASM 2.0 file and integer number of shots.
 It computes the quantum sate before measurement as a complex vector
 and produces the number of counts for each possible measurement outcome.
 """
+
+# Quantum Gates
+I = np.array([[1, 0], [0, 1]])
+X = np.array([[0, 1], [1, 0]])
+Y = np.array([[0, -1j], [1j, 0]])
+Z = np.array([[1, 0], [0, -1]])
+T = np.array([[1, 0], [0, np.exp(1j / 4 * np.pi)]])
+S = np.array([[1, 0], [0, 1j]])
+T_dag = np.array([[1, 0], [0, np.exp(-1j / 4 * np.pi)]])
+S_dag = np.array([[1, 0], [0, -1j]])
+H = 1 / np.sqrt(2) * np.array([[1, 1], [1, -1]])
+
+CNOT = np.array([[1, 0, 0, 0],
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 1],
+                 [0, 0, 1, 0]])
+
+
+def parse_openqasm(file_path):
+    pass
+
+def simulate_quantum_circuit(file_path, shots):
+    pass
+
+def compute_statevector(circuit):
+    pass
+
